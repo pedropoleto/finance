70cca557236ef9433c0855691fb4845c5de89449
Pedro Vitor Poleto <pedropoleto57@gmail.com>
2023-07-08 20:26:07 -0300

    Implementação de mensagens ao usuario

--- perfil/views.py
+++ perfil/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Conta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def home(request):
     return render(request, 'home.html')
@@ -6,3 +9,27 @@ def home(request):
 
 def gerenciar(request):
     return render(request, 'gerenciar.html')
+
+def cadastrar_banco(request):
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.FILES.get('icone')
+    
+    if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
+        messages.add_message(request, constants.ERROR, 'Preencha todos os campos!')
+        return redirect('/perfil/gerenciar')
+
+    conta = Conta(
+        apelido=apelido,
+        banco=banco,
+        tipo=tipo,
+        valor=valor,
+        icone=icone
+    )
+
+    conta.save()
+    messages.add_message(request, constants.SUCCESS, 'Dados salvos com sucesso!')
+
+    return redirect('/perfil/gerenciar')
